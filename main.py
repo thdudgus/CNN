985453935fba131b425af297c1de414c5b89b273
thdudgus <gusthdud527@cau.ac.kr>
2024-11-27 23:10:45 +0900

    resNet 초기 실행

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 # device = 'cpu'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-torch.cuda.is_available()
 print(torch.cuda.is_available())  # True면 CUDA 사용 가능
 print('Current Device : {}'.format(device))
 
@@ -79,67 +78,180 @@ for plot_idx in range(10):
 # 플롯 표시
 plt.show()
 
+# Residual block 구조 정의
+class BasicBlock(nn.Module):
+    mul=1
+    def __init__(self, in_planes, out_planes, stride = 1):
+        super(BasicBlock, self).__init__()
 
-class MyModel(nn.Module):
-    def __init__(self, img_size=32, num_class=100):
-        super(MyModel, self).__init__()
+        # stride를 통해 너비와 높이 조정
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias= False)
+        self.bn1 = nn.BatchNorm2d(out_planes)
 
-        self.img_size = img_size
-        self.num_class = num_class
+        # stride = 1, padding = 1이므로 너비와 높이는 항시 유지됨.
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
 
-        # Convolutional Layers
-        self.features = nn.Sequential(
-            # Conv Layer Block 1
-            nn.Conv2d(3, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # Reduce image size by half
+        # x를 그대로 더해주기 위함.
+        self.shortcut == nn.Sequential()
 
-            # Conv Layer Block 2
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),  # Reduce image size by half
+        # 만약 size가 안 맞아 합 연산이 불가하다면, 연산이 가능하도록 모양을 맞춰 줌
+        if stride != 1: # x와(
+            self.shortcut == nn.Sequential(
+                nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_planes)
+            )
 
-            # Conv Layer Block 3
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)  # Reduce image size by half
-        )
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += self.shortcut(x) # 필요에 따라 layer를 Skip
+        out = F.relu(out)
+        return out
 
-        # Fully Connected Layers
-        self.classifier = nn.Sequential(
-            nn.Linear(256 * 4 * 4, 1024),  # 4x4은 이미지 크기 감소
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),  # Dropout for regularization
-            nn.Linear(1024, 512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(0.5),
-            nn.Linear(512, self.num_class)  # CIFAR-100은 100개의 클래스
-        )
+class BottleNeck(nn.Module):
+    mul = 4
+    def __init__(self, in_planes, out_planes, stride=1):
+        super(BottleNeck, self).__init__()
 
-    def forward(self, img):
-        # Feature extraction
-        features = self.features(img)  # Conv layers 통과
-        features = features.view(features.size(0), -1)  # Flatten
-        # Classification
-        out = self.classifier(features)  # Fully connected layers 통과
+        #첫 Convolution은 너비와 높이 downsampling
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_planes)
+
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+
+        self.conv3 = nn.Conv2d(out_planes, out_planes*self.mul, kernel_size=1, stride=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_planes*self.mul)
+
+        self.shortcut = nn.Sequential()
+
+        if stride != 1 or in_planes != out_planes*self.mul:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, out_planes*self.mul, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_planes*self.mul)
+            )
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = F.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+        out += self.shortcut(x)
+        out = F.relu(out)
         return out
 
 
+class MyModel(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=100):
+        super(MyModel, self).__init__()
+        #RGB 3개채널에서 64개의 Kernel 사용
+        self.in_planes = 64
+
+        # Resnet 논문 구조 그대로 구현
+        self.conv1 = nn.Conv2d(3, self.in_planes, kernel_size=7, stride=2, padding = 3)
+        self.bn1 = nn.BatchNorm2d(self.in_planes)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self.make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self.make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self.make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self.make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.linear = nn.Linear(512 * block.mul, num_classes)
+
+    def make_layer(self, block, out_planes, num_blocks, stride):
+        # layer 앞부분에서만 크기를 절반으로 줄이므로, 아래와 같은 구조
+        strides = [stride] + [1] * (num_blocks-1)
+        layers = []
+        for i in range(num_blocks):
+            layers.append(block(self.in_planes, out_planes, strides[i]))
+            self.in_planes = block.mul * out_planes
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.maxpool1(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out,1)
+        out = self.linear(out)
+        return out
+
+
+def ResNet18():
+    return MyModel(BasicBlock, [2, 2, 2, 2])
+
+def ResNet34():
+    return MyModel(BasicBlock, [3, 4, 6, 3])
+
+def ResNet50():
+    return MyModel(BottleNeck, [3, 4, 6, 3])
+
+def ResNet101():
+    return MyModel(BottleNeck, [3, 4, 23, 3])
+
+def ResNet152():
+    return MyModel(BottleNeck, [3, 8, 36, 3])
+
+# Simple Learning Rate Scheduler
+def lr_scheduler(optimizer, epoch):
+    lr = learning_rate
+    if epoch >= 50:
+        lr /= 10
+    if epoch >= 100:
+        lr /= 10
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+# Xavier
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        #torch.nn.init.xavier_uniform(m.weight)
+        torch.nn.init.kaiming_uniform(m.weight)
+        m.bias.data.fill_(0.01)
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=8)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=256, shuffle=False, num_workers=8)
+
+
 # 모델 인스턴스를 생성
-model = MyModel()
+model = ResNet101()
+# ResNet18, ResNet34, ResNet50, ResNet101, ResNet152 중에 택일하여 사용
+model.apply(init_weights)
+
 # 모델을 선택한 디바이스(CPU 또는 GPU)로 이동
 model = model.to(device)
 
+learning_rate = 0.01
 # 모델 파라미터를 업데이트할 옵티마이저 정의 (여기서는 SGD 사용)
-optimizer = optim.SGD(model.parameters(), lr=1e-1)  # 학습률(lr)을 0.0001로 설정
+#optimizer = optim.SGD(model.parameters(), lr=1e-1)  # 학습률(lr)을 0.0001로 설정
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 # 모델 아키텍처 출력
 print(model)
@@ -196,7 +308,7 @@ def test(model, sample):
     return pred_loss.item(), num_correct.item()
 
 
-max_epoch = 60  # 최대 학습 에폭 수 설정
+max_epoch = 100  # 최대 학습 에폭 수 설정
 
 # 학습 및 테스트 손실을 저장할 리스트 초기화
 tr_loss_saver = []
